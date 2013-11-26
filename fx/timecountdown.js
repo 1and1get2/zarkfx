c6faa503b68d7def858e259c1487e5c5239d4077
duoduo <duoduo3_69@163.com>
2013-11-26 14:35:29 +0800

    添加timecountdown 和tooltip的注释

--- fx/timecountdown.js
+++ fx/timecountdown.js
@@ -1,8 +1,80 @@
 /*
  * DOC_BEGIN
  *
-    year,Month,Day,Hour,Minute,Second,month,day,hour,minute,second
-    
+ * Timecountdown
+ * =========
+ *
+ * 你要组织的晚会、备战高考、需要倒计时么？
+ *
+ * 使用timecountdown可以轻松的实现倒计时的功能。
+ *
+ * 此fx由
+ * `duoduo <http://my.oschina.net/duoduo3369/blog>`_
+ * 开发。
+ *
+ * Options
+ * --------------
+ *
+ * :FX name: timecountdown
+ * :Description: 倒计时
+ * :模版字符串: template参数的类型是"模版字符串"，表示在显示时字符串中用"{}"包裹的参数会被替换为具体的值。比如默认的template='{Day}天{hour}小时{minute}分'会被替换成具体的83天2小时29秒。模版中可以使用的参数有: year,Month,Day,Hour,Minute,Second,month,day,hour,minute,second。大写的表示'总共'，例如'{year}年{month}月'会替换成1年5个月,'{year}年{Month}月',则会替换成1年17个月,当用大写参数时，保证大写参数是第一个，以之前的这个例子只要使用{Month}就可以指明还有17个月了。
+ *
+ * .. topic:: Arguments
+ *
+ *    .. list-table::
+ *       :widths: 1 1 4 1 2
+ *       :header-rows: 1
+ *
+ *       * - Param
+ *         - R/O
+ *         - Description
+ *         - Default
+ *         - Values
+ *
+ *       * - targetTime
+ *         - required
+ *         - 倒计时的目标时间,使用targetTime与用户的浏览器当前时间的差值来倒计时，如果用户浏览器时间不准确,倒计时时间会不正确。
+ *         - default
+ *         - 字符串 例如：'2013/12/25 15:00:00'
+ *
+ *       * - totalSeconds
+ *         - optional
+ *         - 离倒计时的目标时间的总秒数，优先级高于targetTime,如果设定了totalSeconds则targetTime失效。一般是服务器返回一次totalSeconds，这样可以避免用户浏览器时间不准确的问题。
+ *         - default
+ *         - 字符串 例如：'20000'
+ *
+ *       * - style
+ *         - optional
+ *         - 样式
+ *         - none
+ *         - none 
+ *
+ *       * - template
+ *         - optional
+ *         - 倒计时文字模版。
+ *         - '{Day}天{hour}小时{minute}分'
+ *         - 字符串模版,可以随意指定文字，例如:'离高考还有{Day}天'。模板中{}可选择的参数 year,Month,Day,Hour,Minute,Second,month,day,hour,minute,second。
+ *
+ *
+ * 默认效果
+ * -------------------------
+ *
+ * .. zarkfx:: :demo:
+ *
+ *      <div fx="timecountdown[targetTime=2021/12/26 20:00:00;]"></div>
+ *
+ * 使用totalSeconds
+ * -------------------------
+ *
+ * .. zarkfx:: :demo:
+ *      <div fx="timecountdown[targetTime=2021/12/26 20:00:00;totalSeconds=150;]"></div>
+ *
+ * 自定义模板
+ * -------------------------
+ *
+ * .. zarkfx:: :demo:
+ *      <div fx="timecountdown[targetTime=2021/12/20;template=离2021年12月20日还有{Day}天;]"></div>
+ * 
  * DOC_END
  * */
 
--- fx/tooltip.js
+++ fx/tooltip.js
@@ -1,7 +1,111 @@
 /*
  * DOC_BEGIN
  *
-    explode,blind,bounce,clip,drop,fade,fold,highlight,puff,pulsate,scale,shake,slide
+ * Tooltip
+ * ==============================
+ *
+ * 鼠标经过某个元素的时候想要有悬浮提示效果？试试tooltip吧。
+ *
+ * 此fx基于
+ * `jQueryui tooltip <http://jqueryui.com/tooltip/>`_,
+ * 由
+ * `duoduo <http://my.oschina.net/duoduo3369/blog>`_
+ * 开发。
+ *
+ *
+ * Options
+ * --------------
+ *
+ * :FX name: tooltip
+ * :Description: 悬浮提示,使用此fx的元素必须指明title属性,否则没有效果。
+ *
+ * .. topic:: Arguments
+ *
+ *    .. list-table::
+ *       :widths: 1 1 4 1 2
+ *       :header-rows: 1
+ *
+ *       * - Param
+ *         - R/O
+ *         - Description
+ *         - Default
+ *         - Values
+ *
+ *       * - style
+ *         - optional
+ *         - 样式
+ *         - smoothness
+ *         - none 
+ *
+ *       * - content
+ *         - optional
+ *         - 提示框内的内容
+ *         - ''
+ *         - 字符串
+ *
+ *       * - tr
+ *         - optional
+ *         - 动画效果。
+ *         - fade
+ *         - none,fade,explode,blind,bounce,clip,drop,fold,highlight,puff,pulsate,scale,shake,slide
+ *
+ *       * - duration
+ *         - optional
+ *         - 提示消失效果持续的时间。
+ *         - 300
+ *         - 毫秒
+ *
+ *       * - myH
+ *         - optional
+ *         - 用弹出提示框水平的哪个位置去依靠使用此fx元素
+ *         - center
+ *         - left | center | right
+ *
+ *       * - atH
+ *         - optional
+ *         - 弹出提示框在水平位置上依靠在使用此fx元素的哪个位置
+ *         - center
+ *         - left | center | right
+ *
+ *       * - myV
+ *         - optional
+ *         - 用弹出提示框垂直的哪个位置去依靠使用此fx元素
+ *         - center
+ *         - top | center | bottom
+ *
+ *       * - atV
+ *         - optional
+ *         - 弹出提示框在垂直位置上依靠在使用此fx元素的哪个位置
+ *         - center
+ *         - top | center | bottom
+ *
+ *       * - collisionH
+ *         - optional
+ *         - 
+ *         - flip
+ *         - 
+ *
+ *       * - collisionV
+ *         - optional
+ *         - 
+ *         - flip
+ *         - 
+ *
+ * :提示: 你还可以给myH、myV、atH、atV加上偏移量，比如 myH=left+10
+ *
+ * 默认效果
+ * -------------------------
+ *
+ * .. zarkfx:: :demo:
+ *
+ *      <div title="title" fx="tooltip[content=提示框内容;]">鼠标移上来提示</div>
+ *
+ * 定位提示框
+ * -------------------------
+ *
+ * .. zarkfx:: :demo:
+ *      <div title="title" fx="tooltip[content=提示;atH=left+30;atV=center+50;myH=center+20;myV=center+20]">提示</div>
+ *
  * DOC_END
  * */
 
@@ -19,12 +123,10 @@ FX.register('tooltip', [ 'jqueryui-1.10.2','jqueryui-1.10.2/position','jqueryui-
     collisionV   : 'flip'
 
 }, function(attrs){
-    var $this = $(this);  
-    
+    var $this = $(this);
     if(attrs.tr){
         attrs.hide = {"effect":attrs.tr,"duration":attrs.duration};
     }
-    
     if(attrs.myH || attrs.atH || attrs.myV || attrs.atV || attrs.collisionH|| attrs.collisionV){
         attrs.position = {
             "of":$this,
@@ -34,6 +136,5 @@ FX.register('tooltip', [ 'jqueryui-1.10.2','jqueryui-1.10.2/position','jqueryui-
         }
     }
     $($this).tooltip(attrs);
-    
 });
 })();
